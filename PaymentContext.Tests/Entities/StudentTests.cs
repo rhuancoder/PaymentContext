7f9e396a233f434ff0fd9df50b8be222a39f580d
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2018-02-22 17:37:50 -0300

    Refacting

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AddSub()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Rhuan", "Carvalho", "123456789", "rsjlcarvalho@gmail.com");
+            
+            student.AddSubscription(subscription);
+        }
+    }
+}
